07c274ad48ad34e8cc6f0918a5e07827a3d4bace
Lennon-pro <kbjwdh@gmail.com>
2016-07-06 23:24:53 +0800

    init

---
+++ Pods/Target Support Files/Pods-ReSwift-Counter/Pods-ReSwift-Counter-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_ReSwift_CounterVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_ReSwift_CounterVersionString[];
+
---
+++ ReSwift-Counter/AppState.swift
@@ -0,0 +1,15 @@
+//
+//  AppState.swift
+//  ReSwift-Counter
+//
+//  Created by 柯秉钧 on 16/7/6.
+//  Copyright © 2016年 柯秉钧. All rights reserved.
+//
+
+import Foundation
+import ReSwift
+import ReSwiftRouter
+struct AppState: StateType {
+	var counter: Int = 0
+	var navigationState = NavigationState()
+}
